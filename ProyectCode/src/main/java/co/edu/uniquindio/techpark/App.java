9b43e96189f1ff70c3f43ad77fc35faf717e2ca3
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2026-04-29 14:36:50 -0500

    App real real

--- ProyectCode/src/main/java/co/edu/uniquindio/techpark/App.java
+++ ProyectCode/src/main/java/co/edu/uniquindio/techpark/App.java
@@ -1,12 +1,7 @@
 package co.edu.uniquindio.techpark;
 
-<<<<<<< HEAD
-=======
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
->>>>>>> main
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
